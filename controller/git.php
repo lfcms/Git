5b6a4a994c5f9932fa7bd8b3b4baf8bcead8b760
dev <dev@dev5.bioshazard.com>
2013-10-26 11:47:48 -0400

    last

--- controller/git.php
+++ controller/git.php
@@ -316,7 +316,11 @@ Commits:
 		echo '/usr/bin/git '.$match[1].' '.escapeshellarg($_POST['remote']).' '.escapeshellarg($_POST['branch']).'<br />';
 		if($_POST['branch'] != 'master')
 			echo nl2br(shell_exec('/usr/bin/git checkout -b '.escapeshellarg($_POST['branch']).' 2>&1'));
-		echo substr(nl2br(shell_exec('/usr/bin/git '.$match[1].' '.escapeshellarg($_POST['remote']).' '.escapeshellarg($_POST['branch']).' 2>&1')), 0, -1);
+		
+		$tags = '';
+		if($match[1] == 'push') $tags = ' --all';
+		
+		echo substr(nl2br(shell_exec('/usr/bin/git '.$match[1].' '.escapeshellarg($_POST['remote']).' '.escapeshellarg($_POST['branch']).''.$tags.' 2>&1')), 0, -1);
 		echo '</span>';
 		
 		$this->main($vars);
