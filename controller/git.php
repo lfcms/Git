1470da6ece5ac193b67d77991182cb3fc8263490
dev <dev@dev5.bioshazard.com>
2014-03-20 19:07:05 -0400

    'mvc main'

--- controller/git.php
+++ controller/git.php
@@ -100,112 +100,10 @@ class git extends app
 			
 			';
 		
-		
 		$branches = shell_exec('/usr/bin/git for-each-ref --sort=-committerdate refs/heads/');
-	
 		$branches = explode("\n", $branches, -1);
 		
-		?>
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		<div id="git_tools">
-			<h3>Tools</h3>
-			<form action="%appurl%" method="post">
-				<a title="Click to manage your repositories" href="%appurl%repo">Repo</a>: 
-				<select name="newgitpath" />
-					<?php if(is_dir(ROOT.'../.git')) : ?>
-					<optgroup label="System">
-						<option value="<?=ROOT;?>..">LittlefootCMS</option>
-					</optgroup>
-					<?php endif; ?>
-					<optgroup label="Apps"><?=$app_options;?></optgroup>
-					<optgroup label="Skins"><?=$skin_options;?></optgroup>
-				</select> 
-				<input type="submit" value="Change Repo" />
-			</form>
-			<form action="%appurl%pushpull" method="post">
-				<a href="%appurl%remotes">Remotes</a>: <select name="remote" id=""><?=$remotes;?></select> 
-					/ <input type="text" name="branch" placeholder="master" />
-					<input type="submit" name="direction" value="pull" />
-					<input type="submit" name="direction" value="push" /> 
-			</form>
-			<form action="%appurl%tag" method="post">
-				<a href="%appurl%tags">Tag</a>: <input type="text" name="tag" placeholder="Tag (STABLE, DEV)" />
-			</form>
-		</div>
-		
-		<h3>Branches</h3>
-		<form action="%appurl%create" method="post">Create a new branch: <input type="text" name="newbranch" placeholder="New branch name"/> <input type="submit" value="Create" /></form>
-			<ul>
-				<?php if($current == NULL): ?>
-				<li><form action="%appurl%commit" method="post"><strong>Not currently on any branch</strong> <input type="text" name="commit_text" placeholder="Commit text"/> <input type="submit" value="Commit" /><?=$pull;?> <span><?=$branch;?><span></form></li>
-				<?php endif; ?>
-		
-		
-		
-		
-		
-		<?php
-				
-		
-		
-		
-		
-		
-		
-		foreach($branches as $branch)
-		{
-			$pull = ''; 
-			$parts = explode('/', $branch);
-			$branch = substr($branch, 0, 7);
-			/*if($parts[2] != 'master') $pull = '
-				<form action="%appurl%pullrequest/'.$parts[2].'">
-					<input type="text" name="ticketid" placeholder="Ticket ID" />
-					<input type="submit" value="Submit pull request" />
-				</form>';*/
-			
-			if($parts[2] == $current) 
-			{
-				echo '<li class="git_current_branch">
-					<form action="%appurl%commit" method="post">
-						<strong>'.$parts[2].'</strong> 
-						<input type="text" name="commit_text" placeholder="Commit text"/> 
-						<input type="submit" value="Commit" />'.$pull.' 
-						<span style="float: right">(<a href="%appurl%history">view history</a>) '.$branch.'</span></form>
-						<div class="git_current_tools">'.$update.'</div>
-				</li>';
-			}
-			else
-			{
-				$delete = '';
-				if($parts[2] != 'master') 
-					$delete = ' [<a '.jsprompt('Are you sure you want to delete ['.$parts[2].']?').'  href="%appurl%rm/'.$parts[2].'">Delete</a>]';
-				echo '<li><a href="%appurl%checkout/'.$parts[2].'">'.$parts[2].'</a> '.$delete.$pull.'<span style="float: right">'.$branch.'</span></li>';
-			}
-		}
-		echo '</ul>';
-		
-		//echo /*$current.' '.*/$update; 
-		echo '<h4>Status</h4>';
-		echo nl2br($status);
-		
-		
-		// print any message set before last call, then unset
-		if(isset($_SESSION['git_msg']))
-		{
-			echo '<span class="git_msg">';
-			echo nl2br($_SESSION['git_msg']);
-			echo '</span>';
-			unset($_SESSION['git_msg']);
-		}
+		include 'view/git.main.php';
 	}
 	
 	/*public function delete($args)
@@ -303,8 +201,9 @@ class git extends app
 		chdir($this->path);
 		
 		
-		$_SESSION['git_msg'] = substr(nl2br(shell_exec('/usr/bin/git add '.escapeshellcmd($_GET['file']).' 2>&1')), 0, -1);
-		echo '</span>';
+		shell_exec('/usr/bin/git add '.escapeshellcmd($_GET['file']).' 2>&1');
+		
+		$_SESSION['git_msg'] = $_GET['file'].' added to git';
 		
 		redirect302();
 		
---
+++ view/git.main.php
@@ -0,0 +1,80 @@
+<div id="git_tools">
+	<h3>Tools</h3>
+	<form action="%appurl%" method="post">
+		<a title="Click to manage your repositories" href="%appurl%repo">Repo</a>: 
+		<select name="newgitpath" />
+			<?php if(is_dir(ROOT.'../.git')) : ?>
+			<optgroup label="System">
+				<option value="<?=ROOT;?>..">LittlefootCMS</option>
+			</optgroup>
+			<?php endif; ?>
+			<optgroup label="Apps"><?=$app_options;?></optgroup>
+			<optgroup label="Skins"><?=$skin_options;?></optgroup>
+		</select> 
+		<input type="submit" value="Change Repo" />
+	</form>
+	<form action="%appurl%pushpull" method="post">
+		<a href="%appurl%remotes">Remotes</a>: <select name="remote" id=""><?=$remotes;?></select> 
+			/ <input type="text" name="branch" placeholder="master" />
+			<input type="submit" name="direction" value="pull" />
+			<input type="submit" name="direction" value="push" /> 
+	</form>
+	<form action="%appurl%tag" method="post">
+		<a href="%appurl%tags">Tag</a>: <input type="text" name="tag" placeholder="Tag (STABLE, DEV)" />
+	</form>
+</div>
+
+<h3>Branches</h3>
+<form action="%appurl%create" method="post">
+	Create a new branch: <input type="text" name="newbranch" placeholder="New branch name"/> 
+	<input type="submit" value="Create" />
+</form>
+<ul>
+	<?php if($current == NULL): ?>
+	<li><form action="%appurl%commit" method="post"><strong>Not currently on any branch</strong> <input type="text" name="commit_text" placeholder="Commit text"/> <input type="submit" value="Commit" /><?=$pull;?> <span><?=$branch;?><span></form></li>
+	<?php endif; 
+	
+	foreach($branches as $branch)
+	{
+		$pull = ''; 
+		$parts = explode('/', $branch);
+		$branch = substr($branch, 0, 7);
+		/*if($parts[2] != 'master') $pull = '
+			<form action="%appurl%pullrequest/'.$parts[2].'">
+				<input type="text" name="ticketid" placeholder="Ticket ID" />
+				<input type="submit" value="Submit pull request" />
+			</form>';*/
+		
+		if($parts[2] == $current) 
+		{
+			?><li class="git_current_branch">
+				<form action="%appurl%commit" method="post">
+					<strong><?=$parts[2];?></strong> 
+					<input type="text" name="commit_text" placeholder="Commit text"/> 
+					<input type="submit" value="Commit" /><?=$pull;?>
+					<span style="float: right">(<a href="%appurl%history">view history</a>) <?=$branch;?></span></form>
+					<div class="git_current_tools"><?=$update;?></div>
+			</li><?php
+		}
+		else
+		{
+			$delete = '';
+			if($parts[2] != 'master') 
+				$delete = ' [<a '.jsprompt('Are you sure you want to delete ['.$parts[2].']?').'  href="%appurl%rm/'.$parts[2].'">Delete</a>]';
+			echo '<li><a href="%appurl%checkout/'.$parts[2].'">'.$parts[2].'</a> '.$delete.$pull.'<span style="float: right">'.$branch.'</span></li>';
+		}
+	}?>
+</ul>
+		
+<h4>Status</h4>
+<?php 
+echo nl2br($status);
+
+// print any message set before last call, then unset
+if(isset($_SESSION['git_msg']))
+{
+	echo '<span class="git_msg">';
+	echo nl2br($_SESSION['git_msg']);
+	echo '</span>';
+	unset($_SESSION['git_msg']);
+}?>
